d9fee811fcf197502ad43978fd1ea00c1c33d3df
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2025-09-04 10:37:37 +0530

    Create Sample.java
    Create a Sample code.

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class WelcomeToJumanji{
+  public static void main(String[] args)
+  {
+    System.out.println("Hey Soundarya");
+    System.out.println("Hey Rahul");
+    System.out.println("Hey Priyanka");
+  }
+}
