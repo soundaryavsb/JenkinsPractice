37f47d5e41043663ce04e7a5c07967b7728a6956
Soundarya <31506228+soundaryavsb@users.noreply.github.com>
2025-09-09 20:11:32 +0530

    Update Sample.java
    Added "C:\Program Files\Git\bin\git.exe" at Manage Jenkins --> Tools --> Git --> Path : C:\Program Files\Git\bin\git.exe

--- Sample.java
+++ Sample.java
@@ -4,6 +4,7 @@ public class WelcomeToJumanji{
     System.out.println("Hey Soundarya");
     System.out.println("Hey Rose");
     System.out.println("Hey Pinky");
+    System.out.println("ManageJenkins --> Tools --> C:\Program Files\Git\bin\git.exe");
     System.out.println("Integrate Jenkins with Github Successfully!!!");
   }
 }
